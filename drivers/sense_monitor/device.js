b7d4fb5ef5ebe5f0aeea887d2440a5a41a71a6f1
Scott Stogdill <sstogdill@cds-global.com>
2026-09-10 10:07:39 -0500

    fix(device): rebuild app so .homeybuild uses driver.createDevice

--- drivers/sense_monitor/device.js
+++ drivers/sense_monitor/device.js
@@ -189,7 +189,7 @@ class SenseMonitorDevice extends Homey.Device {
           try {
             // In Homey SDK v3, createDevice on driver creates a child device when associated or registered.
             // Let's ensure parent-child linking or standalone driver creation succeeds.
-            await this.homey.drivers.getDriver('sense_monitor').createDevice({
+            await driver.createDevice({
               name: dev.name,
               data: {
                 id: childIdentifier,
